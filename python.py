2dc953d07d0fe61486ce378274fd72cae1e5230a
Danya sri <129587997+danyaadk@users.noreply.github.com>
2023-04-14 11:36:15 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+# Input list of numbers
+list1 =  [12, 14, -95, 3]
+
+# Iterate through the list and print positive numbers
+print("Positive numbers in the list:")
+for num in list1:
+    if num > 0:
+        print(num)
+            
+output:
+Positive numbers in the list: 12,14,3
